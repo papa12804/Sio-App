40d883037a968e7e3d3088de49151797b314db7a
PapaPig <papa12804@gmail.com>
2023-10-12 23:32:23 +0800

    Add MUI.
    Change animation time to 1s in `.App-logo`.
    Using MUI component for UI.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import CycloneIcon from '@mui/icons-material/Cyclone';
+import { ThemeProvider, createTheme, getContrastRatio } from '@mui/material/styles';
 import './App.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#76bc21',
+      contrastText: getContrastRatio('#76bc21', '#fff') > 4.5 ? '#fff' : '#111',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      CPU Fan Speed: <span id="cpu-fan-speed">----</span> rpm.
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            width: 250,
+            height: 200,
+            boxShadow: 1,
+            borderRadius: 2,
+            backgroundColor: 'primary.main',
+            '&:hover': {
+              backgroundColor: 'primary.light',
+              opacity: [0.9, 0.8, 0.7],
+            }
+          }}
+        >
+          <CycloneIcon sx={{color: 'primary.contrastText', fontSize: 40}} className="App-logo"/>
+          <br/>
+          <Box sx={{ color: 'primary.contrastText' }}>
+            CPU Fan Speed:
+            <Box sx={{ color: 'primary.contrastText', fontSize: 34, fontWeight: 'bold' }}>
+              <span id="cpu-fan-speed">----</span> rpm.
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
